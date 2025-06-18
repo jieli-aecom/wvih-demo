247029e808d83463d0f92a9634b4c87e25bea6d3
Jie Li <jie.li@aecom.com>
2025-06-18 14:53:15 -0400

    fix gz

---
+++ assets/useGetComponentWidth-37b1053e.js
@@ -0,0 +1,1 @@
+import{r}from"./index-3669318a.js";function o(){const e=r.useRef(null),[n,s]=r.useState(0);return r.useEffect(()=>{const t=()=>{e.current&&s(e.current.offsetWidth)};return window.addEventListener("resize",t),t(),()=>{window.removeEventListener("resize",t)}},[e]),{ref:e,width:n}}export{o as u};
---
+++ assets/useScreensizeDetector-cce90f08.js
@@ -0,0 +1,1 @@
+import{r}from"./index-3669318a.js";const i=()=>{const[n,e]=r.useState("mobile");return r.useEffect(()=>{const t=()=>{const{innerWidth:s}=window;s>=1079?e("desktop"):s>=767?e("tablet"):e("mobile")};return window.addEventListener("resize",t),t(),()=>{window.removeEventListener("resize",t)}},[]),n},c=i;export{c as u};
